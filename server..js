75f061d4637bc5109732da3e383ac9acf27b4d40
mirkobechini <mirkobechini@gmail.com>
2025-09-04 21:49:06 +0200

    Implement CRUD operations for posts in the Express server

--- server..js
+++ server..js
@@ -2,6 +2,34 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+const posts = [
+    {
+        id: 1,
+        title: "First Post",
+        content: "This is the content of the first post."
+    },
+    {
+        id: 2,
+        title: "Second Post",
+        content: "This is the content of the second post."
+    },
+    {
+        id: 3,
+        title: "Third Post",
+        content: "This is the content of the third post."
+    },
+    {
+        id: 4,
+        title: "Fourth Post",
+        content: "This is the content of the fourth post."
+    },
+    {
+        id: 5,
+        title: "Fifth Post",
+        content: "This is the content of the fifth post."
+    }
+]
+
 app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`);
 })
@@ -12,12 +40,23 @@ app.listen(PORT, () => {
 
 //Index (R)
 app.get("/api/posts", (req, res) => {
-    res.send("Posts")
+    res.send(posts)
 })
 
 //Show (R)
 app.get("/api/posts/:id", (req, res) => {
-    res.send("Show single post with ID:" + req.params.id)
+    
+    const post_id = req.params.id
+    const post = posts.find(post => post.id == parseInt(post_id)) 
+    if(!post){
+        res.status(404)
+        .json({
+            error: true,
+            message: "Post not found"
+        })
+    }else{
+        res.json(post)
+    }
 })
 
 //Store (C)
