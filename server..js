d361507b4ce151ec037fb9299a53b0b7446dcb39
mirkobechini <mirkobechini@gmail.com>
2025-09-04 21:43:19 +0200

    Implement CRUD operations for posts in the Express server

--- server..js
+++ server..js
@@ -5,3 +5,38 @@ const PORT = 3000
 app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`);
 })
+
+
+//CRUD Operation
+
+
+//Index (R)
+app.get("/api/posts", (req, res) => {
+    res.send("Posts")
+})
+
+//Show (R)
+app.get("/api/posts/:id", (req, res) => {
+    res.send("Show single post with ID:" + req.params.id)
+})
+
+//Store (C)
+app.post('/api/posts', (req, res) => {
+    res.send("Create a new post")
+})
+
+//Update (U)
+app.put('/api/posts/:id', (req, res) => {
+    res.send("Update the entire single post with ID:" + req.params.id)
+})
+
+//Modify (U)
+app.patch('/api/posts/:id', (req, res) => {
+    res.send("Partial update the single post with ID:" + req.params.id)
+})
+
+
+//Destroy (D)
+app.delete('/api/posts/:id', (req, res) => {
+    res.send("Delete the single post with ID:" + req.params.id)
+})
