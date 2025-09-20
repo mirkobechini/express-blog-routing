7e4b9ff653df8971eb56a0cdb9c51e01f07189d4
mirkobechini <mirkobechini@gmail.com>
2025-09-20 10:33:32 +0200

    Move posts data to a separate file and update import in posts router

--- routers/posts.js
+++ routers/posts.js
@@ -2,33 +2,8 @@ const express = require ('express')
 const router = express.Router()
 
 
-const posts = [
-    {
-        id: 1,
-        title: "First Post",
-        content: "This is the content of the first post."
-    },
-    {
-        id: 2,
-        title: "Second Post",
-        content: "This is the content of the second post."
-    },
-    {
-        id: 3,
-        title: "Third Post",
-        content: "This is the content of the third post."
-    },
-    {
-        id: 4,
-        title: "Fourth Post",
-        content: "This is the content of the fourth post."
-    },
-    {
-        id: 5,
-        title: "Fifth Post",
-        content: "This is the content of the fifth post."
-    }
-]
+//Import posts data
+const post = require('../data/posts')
 
 //Index (R)
 router.get("/", (req, res) => {
