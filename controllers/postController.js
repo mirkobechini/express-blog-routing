8559d2bc22bd0d789f792db489e87f83e0eb596d
mirkobechini <mirkobechini@gmail.com>
2025-09-21 11:10:23 +0200

    Refactor response handling in index and show functions for consistency

--- controllers/postController.js
+++ controllers/postController.js
@@ -7,8 +7,8 @@ function index(req, res) {
         filtered_posts = posts.filter(post => post.title.includes(req.query.title))
     }
 
-    res.json({
-        status: 200,
+    res.status(200)
+    .json({
         filtered_posts
     })
 }
@@ -18,16 +18,16 @@ function show(req, res) {
     const post_id = parseInt(req.params.id)
     const post = posts.find(post => post.id == post_id) 
     if(!post){
-        res.json({
-            status: 404,
+        res.status(404)
+        .json({
             error: true,
             message: "Post not found"
         })
     }else{
-        res.json({
-            status: 200,
+        res.status(200)
+        .json({
             post
-            })
+        })
     }
 }
 
